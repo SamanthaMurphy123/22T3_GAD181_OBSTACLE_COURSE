feb30ba2972d4353d30ec34847d8c87b973a81af
SamanthaMurphy123 <107165857+SamanthaMurphy123@users.noreply.github.com>
2022-11-14 17:14:59 +1100

    Revert "First Push"
    This reverts commit 1278fb31d25343d0554dc4a93942b777c30ed572.

--- GAD181.2_Samantha_Murphy/Library/Bee/artifacts/emscripten/gqdtm/wrapper_emar.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\2021.3.6f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
-exit %ERRORLEVEL%
--- GAD181.2_Samantha_Murphy/Library/Bee/artifacts/emscripten/vemvw/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"D:\2021.3.6f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
